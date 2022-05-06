c644154aa77ae03ea00b58cce97fbeb40800f3b6
gulshan25 <g.r.tanny@gmail.com>
2022-05-06 15:36:28 +0600

    tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    content: ["./dist/*.{html,js}"],
+    theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+        },
+    },
+    variants: {},
+    plugins: [],
+};
